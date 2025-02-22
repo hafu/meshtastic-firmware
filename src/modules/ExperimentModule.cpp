2d1f06c3b1843fc4c3e6a4f8892fd23c5ef5f50e
Hannes Fuchs <hannes.fuchs@gmx.org>
2025-02-22 22:39:00 +0100

    Also react and send reply message on channel

--- src/modules/ExperimentModule.cpp
+++ src/modules/ExperimentModule.cpp
@@ -2,11 +2,13 @@
 #include "DebugConfiguration.h"
 #include "MeshModule.h"
 #include "MeshService.h"
+#include "MeshTypes.h"
 #include "NodeDB.h"
 #include "RadioInterface.h"
 #include "configuration.h"
 #include "main.h"
 #include "meshtastic/mesh.pb.h"
+#include "modules/NodeInfoModule.h"
 #include <cstddef>
 
 
@@ -15,12 +17,11 @@ ProcessMessage ExperimentModule::handleReceived(const meshtastic_MeshPacket &mp)
     auto &p = mp.decoded;
     LOG_INFO("ExperimentModule: from=0x%0x, to=0x%0x, channel=%u, rx_time=0x%0x, rx_snr=%f, hop_limit=%u, rx_rssi=%i, hop_start=%u, id=0x%x, msg=%.*s", mp.from, mp.to, mp.channel, mp.rx_time, mp.rx_snr, mp.hop_limit, mp.rx_rssi, mp.hop_start, mp.id, p.payload.size, p.payload.bytes);
 
-    // only reply to private msg
-    if (isToUs(&mp) && startsWith(p.payload.bytes, "ping")) {
-        LOG_INFO("ExperimentModule: message for us, send reply.");
+    if (startsWith(p.payload.bytes, "ping")) {
         // TODO: use allocReply implementation?
         meshtastic_MeshPacket *rp = allocDataPacket();
         if (rp) {
+            // per default reply to sender directly
             rp->to = mp.from;
             rp->decoded.want_response = false;
             // TODO: set priority?
@@ -31,14 +32,49 @@ ProcessMessage ExperimentModule::handleReceived(const meshtastic_MeshPacket &mp)
 
             static char replyString[MAX_LORA_PAYLOAD_LEN+1];
             size_t replyStringSize = sizeof(replyString);
-            int nchars;
+            int nchars = 0;
 
-            nchars = snprintf(replyString, replyStringSize, "🤖 pong 🏓 snr: %.2f, rssi: %i, hops: %u", mp.rx_snr, mp.rx_rssi, (mp.hop_start-mp.hop_limit));
+            // private message
+            if (isToUs(&mp)) {
+                nchars = snprintf(
+                                  replyString,
+                                  replyStringSize,
+                                  "🤖 pong 🏓 snr: %.2f, rssi: %i, hops: %u",
+                                  mp.rx_snr, mp.rx_rssi, (mp.hop_start-mp.hop_limit)
+                              );
+            }
+            // TODO: config to only allow to reply directly
+            if (!isFromUs(&mp) && mp.to == NODENUM_BROADCAST) {
+                rp->to = NODENUM_BROADCAST;
+
+                meshtastic_NodeInfoLite *n = nodeDB->getMeshNode(getFrom(&mp));
+                if (n != NULL) {
+                    nchars = snprintf(
+                                      replyString,
+                                      replyStringSize,
+                                      "🤖 @%s pong 🏓 snr: %.2f, rssi: %i, hops: %u",
+                                      n->user.short_name, mp.rx_snr, mp.rx_rssi, (mp.hop_start-mp.hop_limit)
+                                  );
+                } else {
+                    LOG_WARN("ExperimentModule: Node (0x%0x) not in NodeDB", mp.from);
+                    nchars = snprintf(
+                                      replyString,
+                                      replyStringSize,
+                                      "🤖 @!%0x pong 🏓 snr: %.2f, rssi: %i, hops: %u",
+                                      mp.from, mp.rx_snr, mp.rx_rssi, (mp.hop_start-mp.hop_limit)
+                                  );
+                }
+            }
+
             if (nchars > 0 && nchars <= replyStringSize) {
                 rp->decoded.payload.size = strlen(replyString);
                 memcpy(rp->decoded.payload.bytes, replyString, rp->decoded.payload.size);
-                service->sendToMesh(rp);
-                LOG_INFO("ExperimentModule: reply send?");
+                if (airTime->isTxAllowedChannelUtil(true)) {
+                    service->sendToMesh(rp);
+                    LOG_INFO("ExperimentModule: reply send?");
+                } else {
+                    LOG_WARN("ExperimentModule: can not send, air time exceeded");
+                }
             } else {
                 LOG_INFO("ExperimentModule: Buffer overflow? %d available: %d", nchars, replyStringSize);
             }
